da48bd54dceda5120fe0500a14edcaafc1b7a067
Sabir <sabirthapa19@gmail.com>
2022-07-07 21:05:27 +0545

    Initial Commit

---
+++ app1/app1App.swift
@@ -0,0 +1,17 @@
+//
+//  app1App.swift
+//  app1
+//
+//  Created by Sabir's MacBook on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct app1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
